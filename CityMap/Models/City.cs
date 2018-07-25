1b747b65084b19d6085700b97229338ab7e79f8b
MINSK\Siarhei_Mokin <Siarhei_Mokin@epam.com>
2018-07-25 21:02:08 +0300

    initial

---
+++ CityMap/Models/City.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace CityMap.Models
+{
+    public class City
+    {
+        public int id { get; set; }
+        
+        public string title { get; set; }
+        
+        public string description { get; set; }
+        
+        public double latitude { get; set; }
+        
+        public double longitude { get; set; }
+        
+        public string url { get; set; }
+    }
+}
---
+++ CityMap/Models/PhotosContainer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityMap.Models
+{
+    public class PhotosContainer
+    {
+        public IEnumerable<City> photos { get; set; }
+    }
+}
